843d477ea52399d51073c2eb8c437f90d02b232d
Elmona <emil_larsson83@hotmail.com>
2018-11-09 23:07:51 +0100

    Fixing bug with only getting die from 1-5

--- Yahtzee/model/Die.cs
+++ Yahtzee/model/Die.cs
@@ -3,19 +3,19 @@ using System.Runtime.CompilerServices;
 
 namespace Yahtzee.model
 {
-  public class Die
-  {
-    private Random _random;
-    private int _value;
-
-    public Die()
+    public class Die
     {
-      _random = new Random();
-      Throw();
+        private Random _random;
+        private int _value;
+
+        public Die()
+        {
+            _random = new Random();
+            Throw();
+        }
+
+        public int GetValue() => _value;
+
+        public void Throw() => _value = _random.Next(1, 7);
     }
-
-    public int GetValue() => _value;
-
-    public void Throw() => _value = _random.Next(1, 6);
-  }
 }
