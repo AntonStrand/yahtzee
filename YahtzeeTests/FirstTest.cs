3fba805e258e4c057be50028eca4620c00da87c3
antonstrand <jag@antonstrand.se>
2018-11-09 12:27:54 +0100

    Adding init test

---
+++ YahtzeeTests/FirstTest.cs
@@ -0,0 +1,19 @@
+using Xunit;
+using Yahtzee;
+
+namespace MyFirstUnitTests
+{
+    public class FirstTest
+    {
+        [Fact]
+        public void PassingTest()
+        {
+            Assert.Equal(4, Add(2, 2));
+        }
+
+        int Add(int x, int y)
+        {
+            return x + y;
+        }
+    }
+}
