04d106886d1ebdac32fb6c4a81ab3cf07e126bfe
Elmona <emil_larsson83@hotmail.com>
2018-11-09 23:09:11 +0100

    Adding test with rolling 100000 die and check random range

--- YahtzeeTests/DieTest.cs
+++ YahtzeeTests/DieTest.cs
@@ -7,39 +7,57 @@ using Yahtzee.model;
 
 namespace MyFirstUnitTests
 {
-  public class DieTest
-  {
-
-    [Fact]
-    public void GetValueShouldReturnIntBetween1to6()
+    public class DieTest
     {
-      Die sut = new Die();
-      Assert.InRange(sut.GetValue(), 1, 6);
-    }
 
-    [Fact]
-    public void GetSameValueTwiceIfNotThrown()
-    {
-      Die sut = new Die();
-      var expected = sut.GetValue();
-      var actual = sut.GetValue();
-      Assert.Equal(actual, expected);
-    }
+        [Fact]
+        public void GetValueShouldReturnIntBetween1to6()
+        {
+            Die sut = new Die();
+            Assert.InRange(sut.GetValue(), 1, 6);
+        }
 
-    [Fact]
-    public void ThrowShouldGetNewValue()
-    {
-      Die sut = new Die();
-      int startValue = sut.GetValue();
-      List<int> actuals = new List<int>();
-      for (int i = 0; i < 100; i++)
-      {
-        sut.Throw();
-        actuals.Add(sut.GetValue());
-      }
-      var actual = actuals.Count();
-      var unchanged = actuals.Where(val => val == startValue).Count();
-      Assert.NotEqual(actual, unchanged);
+        [Fact]
+        public void GetSameValueTwiceIfNotThrown()
+        {
+            Die sut = new Die();
+            var expected = sut.GetValue();
+            var actual = sut.GetValue();
+            Assert.Equal(actual, expected);
+        }
+
+        [Fact]
+        public void ThrowShouldGetNewValue()
+        {
+            Die sut = new Die();
+            int startValue = sut.GetValue();
+            List<int> actuals = new List<int>();
+            for (int i = 0; i < 100; i++)
+            {
+                sut.Throw();
+                actuals.Add(sut.GetValue());
+            }
+            var actual = actuals.Count();
+            var unchanged = actuals.Where(val => val == startValue).Count();
+            Assert.NotEqual(actual, unchanged);
+        }
+
+        [Fact]
+        public void CheckSoRandomWork()
+        {
+            float count = 100000;
+            var sut = new Die();
+            var dice = new List<int>();
+
+            for (int i = 0; i < count; i++)
+            {
+                sut.Throw();
+                dice.Add(sut.GetValue());
+            }
+
+            var actual = dice.Where(val => val == 6).Count() / count;
+
+            Assert.InRange(actual, 0.12, 0.25);
+        }
     }
-  }
 }
