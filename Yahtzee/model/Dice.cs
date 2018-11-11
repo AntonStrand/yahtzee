2daba3180bc251683bd1c82ffaf9338c246f3b53
antonstrand <jag@antonstrand.se>
2018-11-11 18:49:35 +0100

    GREEN

--- Yahtzee/model/Dice.cs
+++ Yahtzee/model/Dice.cs
@@ -1,18 +1,26 @@
 using System;
-using System.Runtime.CompilerServices;
+using System.Collections.Generic;
 
 namespace Yahtzee.model
 {
   public class Dice
   {
+    private List<Die> _dice;
+
     public Dice()
     {
     }
 
     public Dice(Die d1, Die d2, Die d3, Die d4, Die d5)
     {
+      _dice = new List<Die>();
+      _dice.Add(d1);
+      _dice.Add(d2);
+      _dice.Add(d3);
+      _dice.Add(d4);
+      _dice.Add(d5);
     }
 
-    public void Throw() { }
+    public void Throw() => _dice.ForEach(die => die.Throw());
   }
 }
