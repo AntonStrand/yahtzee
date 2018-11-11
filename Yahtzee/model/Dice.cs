fd00e5a8c47bbfc4c425f1ddd8624f6089e458cd
antonstrand <jag@antonstrand.se>
2018-11-11 19:34:30 +0100

    GREEN

--- Yahtzee/model/Dice.cs
+++ Yahtzee/model/Dice.cs
@@ -13,20 +13,22 @@ namespace Yahtzee.model
       Die4,
       Die5,
     }
-    private List<Die> _dice;
+    private List<Die> _allDice;
+    private List<Die> _throwDice;
 
     public Dice(Die d1, Die d2, Die d3, Die d4, Die d5)
     {
-      _dice = new List<Die>();
-      _dice.Add(d1);
-      _dice.Add(d2);
-      _dice.Add(d3);
-      _dice.Add(d4);
-      _dice.Add(d5);
+      _allDice = new List<Die>();
+      _allDice.Add(d1);
+      _allDice.Add(d2);
+      _allDice.Add(d3);
+      _allDice.Add(d4);
+      _allDice.Add(d5);
+      _throwDice = new List<Die>(_allDice);
     }
 
-    public void Throw() => _dice.ForEach(die => die.Throw());
+    public void Throw() => _throwDice.ForEach(die => die.Throw());
 
-    public void KeepDie(DiceList index) { }
+    public void KeepDie(DiceList index) => _throwDice.RemoveAt(0);
   }
 }
