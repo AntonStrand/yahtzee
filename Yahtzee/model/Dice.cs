fc32f20e7559d71adac06d3792352cb50f803d21
antonstrand <jag@antonstrand.se>
2018-11-11 20:14:22 +0100

    GREEN

--- Yahtzee/model/Dice.cs
+++ Yahtzee/model/Dice.cs
@@ -32,7 +32,7 @@ namespace Yahtzee.model
 
     public void KeepDie(DiceList index) => _keep.Add(_dice[(int)index]);
 
-    public List<int> GetValues() => _dice.Select(_ => 0).ToList();
+    public List<int> GetValues() => _dice.Select(_ => 1).ToList();
 
     private List<Die> GetAvailableDice() => _dice.Except(_keep).ToList();
 
