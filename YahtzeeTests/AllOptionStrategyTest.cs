2713c243c361f52d28e9d9511fb3e7bce4b6b9c5
antonstrand <jag@antonstrand.se>
2018-11-14 20:20:46 +0100

    RED

--- YahtzeeTests/AllOptionStrategyTest.cs
+++ YahtzeeTests/AllOptionStrategyTest.cs
@@ -8,6 +8,23 @@ namespace YahtzeeTests
   public class AllOptionStrategyTest
   {
     [Fact]
+    public void ShouldAcceptDice()
+    {
+      Die die = new DieImplemented();
+      Dice dice = new Dice(die, die, die, die, die);
+      var sut = new AllOptionStrategy();
+
+      try
+      {
+        sut.GetOptions(dice);
+        Assert.True(true);
+      }
+      catch (System.Exception)
+      {
+        Assert.True(false, "Should not throw Exception");
+      }
+    }
+
     public void ShouldNotAcceptNull()
     {
       var sut = new AllOptionStrategy();
