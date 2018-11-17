776a215fd57b5a1ff5b06c2b731ab205d6926b1b
antonstrand <jag@antonstrand.se>
2018-11-17 18:47:48 +0100

    Green

--- Yahtzee/model/category/Pair.cs
+++ Yahtzee/model/category/Pair.cs
@@ -4,12 +4,14 @@ namespace Yahtzee.model.category
 {
   public class Pair : Category
   {
+    private int _value;
     public Pair(int fst, int snd)
     {
       if (!IsMatch(fst, snd)) throw new ArgumentException();
+      _value = fst + snd;
     }
 
-    public int GetValue() => 8;
+    public int GetValue() => _value;
 
     private bool IsMatch(int fst, int snd) => fst == snd;
   }
