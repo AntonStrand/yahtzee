bab90c98e689bfa840cd6dd610c44fa9bca3497f
antonstrand <jag@antonstrand.se>
2018-11-18 15:44:38 +0100

    Refactor

--- YahtzeeTests/TwoPairTest.cs
+++ YahtzeeTests/TwoPairTest.cs
@@ -8,45 +8,31 @@ namespace YahtzeeTests
   public class TwoPairTest
   {
     [Fact]
-    public void ShouldNotAcceptFirstPairToBeNull()
-    {
-      Assert.Throws<ArgumentNullException>(() => new TwoPair(null, new Pair(1, 1)));
-    }
+    public void ShouldNotAcceptFirstPairToBeNull() => AssertArgumentNullException(null, new Pair(1, 1));
 
     [Fact]
-    public void ShouldNotAcceptSecondPairToBeNull()
-    {
-      Assert.Throws<ArgumentNullException>(() => new TwoPair(new Pair(1, 1), null));
-    }
+    public void ShouldNotAcceptSecondPairToBeNull() => AssertArgumentNullException(new Pair(1, 1), null);
 
     [Fact]
-    public void ShouldReturnValueOfBothPairs()
-    {
-      var sut = SetupValueTest(4, 6);
-
-      var expected = 10;
-      var actual = sut.GetValue();
-      Assert.Equal(expected, actual);
-    }
+    public void ShouldReturnValueOfBothPairs() => AssertValue(10, 4, 6);
 
     [Fact]
-    public void ShouldReturnValueOfBothPairs1()
-    {
-      var sut = SetupValueTest(12, 10);
+    public void ShouldReturnValueOfBothPairs1() => AssertValue(22, 12, 10);
 
-      var expected = 22;
-      var actual = sut.GetValue();
-      Assert.Equal(expected, actual);
-    }
-
-    private TwoPair SetupValueTest(int p1Value, int p2Value)
+    private void AssertValue(int expected, int p1Value, int p2Value)
     {
       var input1 = new Mock<Pair>(1, 1);
       var input2 = new Mock<Pair>(2, 2);
       input1.Setup(pair => pair.GetValue()).Returns(p1Value);
       input2.Setup(pair => pair.GetValue()).Returns(p2Value);
 
-      return new TwoPair(input1.Object, input2.Object);
+      var sut = new TwoPair(input1.Object, input2.Object);
+
+      var actual = sut.GetValue();
+      Assert.Equal(expected, actual);
     }
+
+    private void AssertArgumentNullException(Pair input1, Pair input2) =>
+      Assert.Throws<ArgumentNullException>(() => new TwoPair(input1, input2));
   }
 }
