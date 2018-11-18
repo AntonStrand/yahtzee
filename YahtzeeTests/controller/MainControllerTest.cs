ca122635cbaafede68db4e3ddace5cc631cc9bf7
Elmona <emil_larsson83@hotmail.com>
2018-11-18 15:07:51 +0100

    Red

--- YahtzeeTests/controller/MainControllerTest.cs
+++ YahtzeeTests/controller/MainControllerTest.cs
@@ -1,16 +1,19 @@
 using System;
 using Xunit;
 using Yahtzee.controller;
+using Yahtzee.view;
 
 namespace YahtzeeTests
 {
   public class MainControllerTest
   {
     [Fact]
-    public void NewMainController()
+    public void NewMainControllerWithView()
     {
-      var controller = new MainController();
-      Assert.IsType<Yahtzee.controller.MainController>(controller);
+      var v = new MainView();
+      var c = new MainController(v);
+      Assert.IsType<Yahtzee.controller.MainController>(c);
     }
+
   }
 }
