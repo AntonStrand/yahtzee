0975de0fc81aa9e55d526d921137aa71cf7dfcfe
antonstrand <jag@antonstrand.se>
2018-11-18 21:40:14 +0100

    Refactor

--- YahtzeeTests/SmallStraightTest.cs
+++ YahtzeeTests/SmallStraightTest.cs
@@ -20,11 +20,7 @@ namespace YahtzeeTests
     public void ShouldNotAcceptMoreThan5Items() => AssertArgumentOutOfRangeException(new List<int>() { 1, 2, 3, 4, 5, 6 });
 
     [Fact]
-    public void ShouldNotAcceptMoreThanOneOfEach()
-    {
-      Assert.Throws<ArgumentException>(() => new SmallStraight(new List<int>() { 1, 1, 1, 1, 1 }));
-
-    }
+    public void ShouldNotAcceptMoreThanOneOfEach() => Assert.Throws<ArgumentException>(() => new SmallStraight(new List<int>() { 1, 1, 1, 1, 1 }));
 
     private void AssertArgumentOutOfRangeException(List<int> input) =>
       Assert.Throws<ArgumentOutOfRangeException>(() => new SmallStraight(input));
