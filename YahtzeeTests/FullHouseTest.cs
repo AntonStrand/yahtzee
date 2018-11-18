63c63887f3636b39a60df4610bfb3ec8bcd48ab1
antonstrand <jag@antonstrand.se>
2018-11-18 13:20:19 +0100

    Red

--- YahtzeeTests/FullHouseTest.cs
+++ YahtzeeTests/FullHouseTest.cs
@@ -13,5 +13,9 @@ namespace YahtzeeTests
     [Fact]
     public void ShouldNotAcceptNull() =>
       Assert.Throws<ArgumentNullException>(() => new FullHouse(null, null));
+
+    [Fact]
+    public void ShouldNotAcceptThreeOfAKindIsNull() =>
+      Assert.Throws<ArgumentNullException>(() => new FullHouse(new Pair(1, 1), null));
   }
 }
