bb2c145454945895046200df2bd8cf9f2f4ddb51
antonstrand <jag@antonstrand.se>
2018-11-18 11:46:39 +0100

    Green

--- Yahtzee/model/category/ThreeOfAKind.cs
+++ Yahtzee/model/category/ThreeOfAKind.cs
@@ -6,7 +6,7 @@ namespace Yahtzee.model.category
   {
     public ThreeOfAKind(int v1, int v2, int v3)
     {
-
+      if (v1 != v2) throw new ArgumentException();
     }
 
     public int GetValue() => 0;
