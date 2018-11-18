449dfd2e2edc4c20afed690c3861836d4cf0d485
Elmona <emil_larsson83@hotmail.com>
2018-11-18 15:26:56 +0100

    Green

--- Yahtzee/controller/MainController.cs
+++ Yahtzee/controller/MainController.cs
@@ -5,7 +5,13 @@ namespace Yahtzee.controller
 {
   public class MainController
   {
+    private MainView mainView;
     public MainController(MainView v)
+    {
+      this.mainView = v;
+    }
+
+    public void Play()
     {
 
     }
