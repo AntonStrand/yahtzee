d11917070fbe8e4d062a799c13025842cd8af9d5
antonstrand <jag@antonstrand.se>
2018-11-18 12:24:38 +0100

    Refactor

--- Yahtzee/model/category/FourOfAKind.cs
+++ Yahtzee/model/category/FourOfAKind.cs
@@ -8,8 +8,7 @@ namespace Yahtzee.model.category
 
     public FourOfAKind(int v1, int v2, int v3, int v4)
     {
-      if (v1 != v2) throw new ArgumentException();
-      if (v3 != v4) throw new ArgumentException();
+      if (v1 != v2 || v3 != v4) throw new ArgumentException();
     }
 
     public int GetValue() => _value;
