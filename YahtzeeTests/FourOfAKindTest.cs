3e6cc6b034036c0d6d3a47ccfd2bcb324679af39
antonstrand <jag@antonstrand.se>
2018-11-18 12:22:04 +0100

    Red

--- YahtzeeTests/FourOfAKindTest.cs
+++ YahtzeeTests/FourOfAKindTest.cs
@@ -7,6 +7,13 @@ namespace YahtzeeTests
   public class FourOfAKindTest
   {
     [Fact]
+    public void ShouldAcceptIfAllMatch()
+    {
+      var input = 1;
+      new FourOfAKind(input, input, input, input);
+    }
+
+    [Fact]
     public void ShouldNotAcceptTheFirstTwoArgumentsBeingDifferent()
     {
       Assert.Throws<ArgumentException>(() => new FourOfAKind(1, 3, 3, 3));
