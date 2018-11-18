ced50c6441d424d1fb90820978d5c91d95262a14
antonstrand <jag@antonstrand.se>
2018-11-18 12:03:59 +0100

    Refactor

--- Yahtzee/model/category/ThreeOfAKind.cs
+++ Yahtzee/model/category/ThreeOfAKind.cs
@@ -8,10 +8,12 @@ namespace Yahtzee.model.category
 
     public ThreeOfAKind(int v1, int v2, int v3)
     {
-      if (v1 != v2 || v2 != v3) throw new ArgumentException();
+      if (IsDifferent(v1, v2, v3)) throw new ArgumentException();
       _value = v1 * 3;
     }
 
     public int GetValue() => _value;
+
+    private bool IsDifferent(int v1, int v2, int v3) => (v1 != v2 || v2 != v3);
   }
 }
