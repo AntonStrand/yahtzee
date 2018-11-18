dc4a82bbfb26ee896b13af837edd0bb0a61ef062
antonstrand <jag@antonstrand.se>
2018-11-18 10:25:35 +0100

    Refactor

--- Yahtzee/model/category/TwoPair.cs
+++ Yahtzee/model/category/TwoPair.cs
@@ -6,8 +6,7 @@ namespace Yahtzee.model.category
   {
     public TwoPair(Pair pair1, Pair pair2)
     {
-      if (pair1 == null) throw new ArgumentNullException();
-      if (pair2 == null) throw new ArgumentNullException();
+      if (pair1 == null || pair2 == null) throw new ArgumentNullException();
     }
     public int GetValue() => 0;
   }
