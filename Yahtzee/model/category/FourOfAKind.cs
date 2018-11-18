0d5e8fdcd0bd3c669dbf69547213264e09bb9b16
antonstrand <jag@antonstrand.se>
2018-11-18 12:46:26 +0100

    Green

--- Yahtzee/model/category/FourOfAKind.cs
+++ Yahtzee/model/category/FourOfAKind.cs
@@ -4,12 +4,15 @@ namespace Yahtzee.model.category
 {
   public class FourOfAKind : Category
   {
+    private int _value;
+
     public FourOfAKind(int v1, int v2, int v3, int v4)
     {
       if (IsDifferent(v1, v2, v3, v4)) throw new ArgumentException();
+      _value = v1 * 4;
     }
 
-    public int GetValue() => 20;
+    public int GetValue() => _value;
 
     private bool IsDifferent(int v1, int v2, int v3, int v4) => (v1 != v2 || v2 != v3 || v3 != v4);
   }
