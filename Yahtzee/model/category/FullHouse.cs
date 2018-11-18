fc8d08941bfa869a55dc378eb03d62fbb60e82fc
antonstrand <jag@antonstrand.se>
2018-11-18 13:20:55 +0100

    Green

--- Yahtzee/model/category/FullHouse.cs
+++ Yahtzee/model/category/FullHouse.cs
@@ -6,7 +6,7 @@ namespace Yahtzee.model.category
   {
     public FullHouse(Pair pair, ThreeOfAKind threeOfAKind)
     {
-      if (pair == null) throw new ArgumentNullException();
+      if (threeOfAKind == null) throw new ArgumentNullException();
     }
 
     public int GetValue() => 0;
