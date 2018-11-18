5c9e37545f46d57b325472bc5a4cb064beb4e744
antonstrand <jag@antonstrand.se>
2018-11-18 17:11:31 +0100

    Red

--- YahtzeeTests/SmallStraight.cs
+++ YahtzeeTests/SmallStraight.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using Xunit;
 using Yahtzee.model.category;
 
@@ -6,6 +7,9 @@ namespace YahtzeeTests
 {
   public class SmallStraightTest
   {
+    [Fact]
+    public void ShouldAcceptCorrectInput() => new SmallStraight(new List<int>() { 1, 2, 3, 4, 5 });
+
     [Fact]
     public void ShouldNotAcceptNull()
     {
