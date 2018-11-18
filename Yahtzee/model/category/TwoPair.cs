cf467a037e43d33f6919027f5c66f8d9622b3089
antonstrand <jag@antonstrand.se>
2018-11-18 11:08:05 +0100

    Green

--- Yahtzee/model/category/TwoPair.cs
+++ Yahtzee/model/category/TwoPair.cs
@@ -4,10 +4,12 @@ namespace Yahtzee.model.category
 {
   public class TwoPair : Category
   {
+    private int _value;
     public TwoPair(Pair pair1, Pair pair2)
     {
       if (pair1 == null || pair2 == null) throw new ArgumentNullException();
+      _value = pair1.GetValue() + pair2.GetValue();
     }
-    public int GetValue() => 10;
+    public int GetValue() => _value;
   }
 }
