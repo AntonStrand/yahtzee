3185cb6873f5380dc5ef98b4ef0e6ca21433112c
antonstrand <jag@antonstrand.se>
2018-11-18 12:22:35 +0100

    Green

--- Yahtzee/model/category/FourOfAKind.cs
+++ Yahtzee/model/category/FourOfAKind.cs
@@ -8,7 +8,7 @@ namespace Yahtzee.model.category
 
     public FourOfAKind(int v1, int v2, int v3, int v4)
     {
-      throw new ArgumentException();
+      if (v1 != v2) throw new ArgumentException();
     }
 
     public int GetValue() => _value;
