72df50c20828e6ee1b99dd8f3d2ce12d0c4eb079
Elmona <emil_larsson83@hotmail.com>
2018-11-20 14:37:57 +0100

    Refactor

--- YahtzeeTests/view/DiceViewTest.cs
+++ YahtzeeTests/view/DiceViewTest.cs
@@ -16,161 +16,38 @@ namespace YahtzeeTests
     [Fact]
     public void ShouldPrintFiveOnes()
     {
-      var die = new Mock<Die>();
-      die.Setup(d => d.GetValue()).Returns(1);
-      var dice = new Dice(die.Object, die.Object, die.Object, die.Object, die.Object);
-
-      var diceView = new DiceView(dice);
-
-      using (StringWriter sw = new StringWriter())
-      {
-        Console.SetOut(sw);
-
-        diceView.Print();
-
-        string expected =
-            " _____    _____    _____    _____    _____   \n"
-         + "|     |  |     |  |     |  |     |  |     |  \n"
-         + "|  o  |  |  o  |  |  o  |  |  o  |  |  o  |  \n"
-         + "|_____|  |_____|  |_____|  |_____|  |_____|  \n";
-
-        Assert.Equal(expected, sw.ToString());
-        sw.Close();
-      }
+      string expected =
+          " _____    _____    _____    _____    _____   \n"
+       + "|     |  |     |  |     |  |     |  |     |  \n"
+       + "|  o  |  |  o  |  |  o  |  |  o  |  |  o  |  \n"
+       + "|_____|  |_____|  |_____|  |_____|  |_____|  \n";
+      AssertView(expected, 1, 1, 1, 1, 1);
     }
 
     [Fact]
-    public void ShouldPrintFiveTwos()
+    public void ShouldPrintAllDicesOneToFive()
     {
-      var die = new Mock<Die>();
-      die.Setup(d => d.GetValue()).Returns(2);
-      var dice = new Dice(die.Object, die.Object, die.Object, die.Object, die.Object);
-
-      var diceView = new DiceView(dice);
-
-      using (StringWriter sw = new StringWriter())
-      {
-        Console.SetOut(sw);
-
-        diceView.Print();
-
-        string expected =
-            " _____    _____    _____    _____    _____   \n"
-         + "|o    |  |o    |  |o    |  |o    |  |o    |  \n"
-         + "|     |  |     |  |     |  |     |  |     |  \n"
-         + "|____o|  |____o|  |____o|  |____o|  |____o|  \n";
-
-        Assert.Equal(expected, sw.ToString());
-        sw.Close();
-      }
+      string expected =
+          " _____    _____    _____    _____    _____   \n"
+       + "|     |  |o    |  |o    |  |o   o|  |o   o|  \n"
+       + "|  o  |  |     |  |  o  |  |     |  |  o  |  \n"
+       + "|_____|  |____o|  |____o|  |o___o|  |o___o|  \n";
+      AssertView(expected, 1, 2, 3, 4, 5);
     }
 
     [Fact]
-    public void ShouldPrintFiveThrees()
+    public void ShouldPrintAllDicesTwoToSix()
     {
-      var die = new Mock<Die>();
-      die.Setup(d => d.GetValue()).Returns(3);
-      var dice = new Dice(die.Object, die.Object, die.Object, die.Object, die.Object);
+      string expected =
+          " _____    _____    _____    _____    _____   \n"
+       + "|o    |  |o    |  |o   o|  |o   o|  |o   o|  \n"
+       + "|     |  |  o  |  |     |  |  o  |  |o   o|  \n"
+       + "|____o|  |____o|  |o___o|  |o___o|  |o___o|  \n";
 
-      var diceView = new DiceView(dice);
-
-      using (StringWriter sw = new StringWriter())
-      {
-        Console.SetOut(sw);
-
-        diceView.Print();
-
-        string expected =
-            " _____    _____    _____    _____    _____   \n"
-         + "|o    |  |o    |  |o    |  |o    |  |o    |  \n"
-         + "|  o  |  |  o  |  |  o  |  |  o  |  |  o  |  \n"
-         + "|____o|  |____o|  |____o|  |____o|  |____o|  \n";
-
-        Assert.Equal(expected, sw.ToString());
-        sw.Close();
-      }
+      AssertView(expected, 2, 3, 4, 5, 6);
     }
 
-    [Fact]
-    public void ShouldPrintFiveFours()
-    {
-      var die = new Mock<Die>();
-      die.Setup(d => d.GetValue()).Returns(4);
-      var dice = new Dice(die.Object, die.Object, die.Object, die.Object, die.Object);
-
-      var diceView = new DiceView(dice);
-
-      using (StringWriter sw = new StringWriter())
-      {
-        Console.SetOut(sw);
-
-        diceView.Print();
-
-        string expected =
-            " _____    _____    _____    _____    _____   \n"
-         + "|o   o|  |o   o|  |o   o|  |o   o|  |o   o|  \n"
-         + "|     |  |     |  |     |  |     |  |     |  \n"
-         + "|o___o|  |o___o|  |o___o|  |o___o|  |o___o|  \n";
-
-        Assert.Equal(expected, sw.ToString());
-        sw.Close();
-      }
-    }
-
-    [Fact]
-    public void ShouldPrintFiveFives()
-    {
-      var die = new Mock<Die>();
-      die.Setup(d => d.GetValue()).Returns(5);
-      var dice = new Dice(die.Object, die.Object, die.Object, die.Object, die.Object);
-
-      var diceView = new DiceView(dice);
-
-      using (StringWriter sw = new StringWriter())
-      {
-        Console.SetOut(sw);
-
-        diceView.Print();
-
-        string expected =
-            " _____    _____    _____    _____    _____   \n"
-         + "|o   o|  |o   o|  |o   o|  |o   o|  |o   o|  \n"
-         + "|  o  |  |  o  |  |  o  |  |  o  |  |  o  |  \n"
-         + "|o___o|  |o___o|  |o___o|  |o___o|  |o___o|  \n";
-
-        Assert.Equal(expected, sw.ToString());
-        sw.Close();
-      }
-    }
-
-    [Fact]
-    public void ShouldPrintFiveSixes()
-    {
-      var die = new Mock<Die>();
-      die.Setup(d => d.GetValue()).Returns(6);
-      var dice = new Dice(die.Object, die.Object, die.Object, die.Object, die.Object);
-
-      var diceView = new DiceView(dice);
-
-      using (StringWriter sw = new StringWriter())
-      {
-        Console.SetOut(sw);
-
-        diceView.Print();
-
-        string expected =
-            " _____    _____    _____    _____    _____   \n"
-         + "|o   o|  |o   o|  |o   o|  |o   o|  |o   o|  \n"
-         + "|o   o|  |o   o|  |o   o|  |o   o|  |o   o|  \n"
-         + "|o___o|  |o___o|  |o___o|  |o___o|  |o___o|  \n";
-
-        Assert.Equal(expected, sw.ToString());
-        sw.Close();
-      }
-    }
-
-    [Fact]
-    public void ShouldPrintAllDices()
+    private void AssertView(string expected, int v1, int v2, int v3, int v4, int v5)
     {
       var die1 = new Mock<Die>();
       var die2 = new Mock<Die>();
@@ -178,11 +55,11 @@ namespace YahtzeeTests
       var die4 = new Mock<Die>();
       var die5 = new Mock<Die>();
 
-      die1.Setup(d => d.GetValue()).Returns(1);
-      die2.Setup(d => d.GetValue()).Returns(2);
-      die3.Setup(d => d.GetValue()).Returns(3);
-      die4.Setup(d => d.GetValue()).Returns(4);
-      die5.Setup(d => d.GetValue()).Returns(5);
+      die1.Setup(d => d.GetValue()).Returns(v1);
+      die2.Setup(d => d.GetValue()).Returns(v2);
+      die3.Setup(d => d.GetValue()).Returns(v3);
+      die4.Setup(d => d.GetValue()).Returns(v4);
+      die5.Setup(d => d.GetValue()).Returns(v5);
 
       var dice = new Dice(die1.Object, die2.Object, die3.Object, die4.Object, die5.Object);
 
@@ -193,12 +70,6 @@ namespace YahtzeeTests
         Console.SetOut(sw);
 
         diceView.Print();
-
-        string expected =
-            " _____    _____    _____    _____    _____   \n"
-         + "|     |  |o    |  |o    |  |o   o|  |o   o|  \n"
-         + "|  o  |  |     |  |  o  |  |     |  |  o  |  \n"
-         + "|_____|  |____o|  |____o|  |o___o|  |o___o|  \n";
 
         Assert.Equal(expected, sw.ToString());
         sw.Close();
