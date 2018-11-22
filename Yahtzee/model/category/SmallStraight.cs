70c4761481a951af0254d4facf15d82f12409827
antonstrand <jag@antonstrand.se>
2018-11-22 12:58:33 +0100

    Refactor

--- Yahtzee/model/category/SmallStraight.cs
+++ Yahtzee/model/category/SmallStraight.cs
@@ -10,16 +10,17 @@ namespace Yahtzee.model.category
     {
       if (values == null) throw new ArgumentNullException();
       if (IsOutOfRange(values)) throw new ArgumentOutOfRangeException();
-      if (HasDoublets(values)) throw new ArgumentException();
-      if (!IsCorrectInput(values)) throw new ArgumentException();
+      if (IsInvalidInput(values)) throw new ArgumentException();
     }
 
     public int GetValue() => 15;
 
     private bool IsOutOfRange(List<int> list) => list.Count != 5;
 
+    private bool IsInvalidInput(List<int> list) =>
+      HasDoublets(list) || list.OrderBy(v => v).Where((value, i) => value != (i + 1)).ToList().Count != 0;
+
     private bool HasDoublets(List<int> list) => list.Distinct().ToList().Count != list.Count;
 
-    private bool IsCorrectInput(List<int> list) => list.OrderBy(v => v).Where((value, i) => value != (i + 1)).ToList().Count == 0;
   }
 }
