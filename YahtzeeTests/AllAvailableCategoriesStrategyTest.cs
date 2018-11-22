a5e153e5e607744c1027455beeeac3e342d36238
antonstrand <jag@antonstrand.se>
2018-11-22 20:44:48 +0100

    Refactor

--- YahtzeeTests/AllAvailableCategoriesStrategyTest.cs
+++ YahtzeeTests/AllAvailableCategoriesStrategyTest.cs
@@ -37,29 +37,17 @@ namespace YahtzeeTests
       Assert.IsType<Pair>(actual);
     }
 
-    [Fact]
-    public void ShouldReturnPairOf6()
+    [Theory]
+    [InlineData(5, 6, 6, 2, 4, 12)]
+    [InlineData(4, 1, 6, 2, 4, 8)]
+    public void ShouldReturnPairFrom(int v1, int v2, int v3, int v4, int v5, int expected)
     {
       var fakePlayer = new Mock<ScoreBoard>();
       var fakeDice = new Mock<Dice>();
-      fakeDice.Setup(d => d.GetValues()).Returns(new List<int>() { 5, 6, 6, 2, 4 });
+      fakeDice.Setup(d => d.GetValues()).Returns(new List<int>() { v1, v2, v3, v4, v5 });
 
       var sut = new AllAvailableCategoriesStrategy();
       var actual = sut.GetCategories(fakeDice.Object, fakePlayer.Object)[0].GetValue();
-      var expected = 12;
-      Assert.Equal(expected, actual);
-    }
-
-    [Fact]
-    public void ShouldReturnPairOf4()
-    {
-      var fakePlayer = new Mock<ScoreBoard>();
-      var fakeDice = new Mock<Dice>();
-      fakeDice.Setup(d => d.GetValues()).Returns(new List<int>() { 4, 1, 6, 2, 4 });
-
-      var sut = new AllAvailableCategoriesStrategy();
-      var actual = sut.GetCategories(fakeDice.Object, fakePlayer.Object)[0].GetValue();
-      var expected = 8;
       Assert.Equal(expected, actual);
     }
   }
