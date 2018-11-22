450ff5529a7f58d0a9045d022b2b0dd5be647a87
antonstrand <jag@antonstrand.se>
2018-11-22 12:55:29 +0100

    Green

--- Yahtzee/model/category/SmallStraight.cs
+++ Yahtzee/model/category/SmallStraight.cs
@@ -11,6 +11,7 @@ namespace Yahtzee.model.category
       if (values == null) throw new ArgumentNullException();
       if (IsOutOfRange(values)) throw new ArgumentOutOfRangeException();
       if (HasDoublets(values)) throw new ArgumentException();
+      if (!IsCorrectInput(values)) throw new ArgumentException();
     }
 
     public int GetValue() => 15;
@@ -18,5 +19,7 @@ namespace Yahtzee.model.category
     private bool IsOutOfRange(List<int> list) => list.Count != 5;
 
     private bool HasDoublets(List<int> list) => list.Distinct().ToList().Count != list.Count;
+
+    private bool IsCorrectInput(List<int> list) => list.OrderBy(v => v).Where((value, i) => value != (i + 1)).ToList().Count == 0;
   }
 }
