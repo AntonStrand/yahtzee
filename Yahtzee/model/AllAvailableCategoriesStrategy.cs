0a80d6f998678db065a458d5b807186d5b917a62
antonstrand <jag@antonstrand.se>
2018-11-22 20:39:54 +0100

    Refactor

--- Yahtzee/model/AllAvailableCategoriesStrategy.cs
+++ Yahtzee/model/AllAvailableCategoriesStrategy.cs
@@ -10,10 +10,17 @@ namespace Yahtzee.model
     public List<Category> GetCategories(Dice dice, ScoreBoard scoreBoard)
     {
       if (IsEitherNull(dice, scoreBoard)) throw new ArgumentNullException();
-      var pairs = dice.GetValues().GroupBy(x => x).ToDictionary(x => x.Key, x => x.Count()).Where(x => x.Value == 2).Select(x => x.Key).Select(v => new Pair(v, v)).ToList();
-      return new List<Category>(pairs);
+      return new List<Category>(GetPairs(dice));
     }
 
     private bool IsEitherNull(Dice dice, ScoreBoard scoreBoard) => dice == null || scoreBoard == null;
+
+    private List<Pair> GetPairs(Dice dice) =>
+      GetFrequencyTable(dice)
+        .Where(x => x.Value == 2)
+        .Select(x => new Pair(x.Key, x.Key))
+        .ToList();
+
+    private Dictionary<int, int> GetFrequencyTable(Dice dice) => dice.GetValues().GroupBy(x => x).ToDictionary(x => x.Key, x => x.Count());
   }
 }
