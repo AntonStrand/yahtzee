68a8b5985f319302131968c5f5251943402612bc
antonstrand <jag@antonstrand.se>
2018-11-25 20:29:12 +0100

    Red

--- YahtzeeTests/YahtzeeTest.cs
+++ YahtzeeTests/YahtzeeTest.cs
@@ -16,6 +16,7 @@ namespace YahtzeeTests
 
     [Theory]
     [InlineData(1, 1, 1, 1, 1)]
+    [InlineData(6, 6, 6, 6, 6)]
     public void ShouldSetValueIfInputIsCorrect(int v1, int v2, int v3, int v4, int v5)
     {
       var sut = new category.Yahtzee(v1, v2, v3, v4, v5);
