10e56b832ec0d7024821c53f5bbdf746abe60d16
antonstrand <jag@antonstrand.se>
2018-11-25 16:42:29 +0100

    Green

--- Yahtzee/model/category/LargeStraight.cs
+++ Yahtzee/model/category/LargeStraight.cs
@@ -6,7 +6,10 @@ namespace Yahtzee.model.category
 {
   public class LargeStraight : Category
   {
-    public LargeStraight(int v1, int v2, int v3, int v4, int v5) { }
+    public LargeStraight(int v1, int v2, int v3, int v4, int v5)
+    {
+      throw new ArgumentException();
+    }
 
     public int GetValue() => 0;
   }
