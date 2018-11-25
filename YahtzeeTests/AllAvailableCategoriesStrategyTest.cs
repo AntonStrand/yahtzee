febcfcd9492f294744a718731f667eaedddd220c
antonstrand <jag@antonstrand.se>
2018-11-25 16:54:00 +0100

    Red

--- YahtzeeTests/AllAvailableCategoriesStrategyTest.cs
+++ YahtzeeTests/AllAvailableCategoriesStrategyTest.cs
@@ -45,6 +45,9 @@ namespace YahtzeeTests
     public void ShouldReturnSmallStraight() => AssertType<SmallStraight>(new List<int>() { 1, 2, 3, 4, 5 });
 
     [Fact]
+    public void ShouldReturnLargeStraight() => AssertType<LargeStraight>(new List<int>() { 2, 3, 4, 5, 6 });
+
+    [Fact]
     public void ShouldOnlyReturnAPair()
     {
       var actual = ExerciseSUT(new List<int>() { 1, 1, 2, 4, 6 }).Where(c => !IsOfType<Pair>(c)).ToList().Count;
