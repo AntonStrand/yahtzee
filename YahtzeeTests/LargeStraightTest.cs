ede42820474a2640e833a2ab482a32cdbdb83618
antonstrand <jag@antonstrand.se>
2018-11-25 16:44:50 +0100

    Red

--- YahtzeeTests/LargeStraightTest.cs
+++ YahtzeeTests/LargeStraightTest.cs
@@ -8,5 +8,13 @@ namespace YahtzeeTests
   {
     [Fact]
     public void ShouldNotAcceptMoreThanOneOfEach() => Assert.Throws<ArgumentException>(() => new LargeStraight(1, 1, 1, 1, 1));
+
+    [Fact]
+    public void ShouldAcceptValidInput()
+    {
+      var sut = new LargeStraight(2, 3, 4, 5, 6);
+      var actual = sut.GetValue();
+      Assert.Equal(20, actual);
+    }
   }
 }
