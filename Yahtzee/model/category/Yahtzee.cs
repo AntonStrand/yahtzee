41bbb0f516233df2f7826ec2dd9f7489d88ef58d
antonstrand <jag@antonstrand.se>
2018-11-25 20:14:08 +0100

    Green

--- Yahtzee/model/category/Yahtzee.cs
+++ Yahtzee/model/category/Yahtzee.cs
@@ -6,7 +6,10 @@ namespace Yahtzee.model.category
 {
   public class Yahtzee : Category
   {
-    public Yahtzee(int v1, int v2, int v3, int v4, int v5) { }
+    public Yahtzee(int v1, int v2, int v3, int v4, int v5)
+    {
+      throw new ArgumentException();
+    }
 
     public int GetValue() => 0;
   }
