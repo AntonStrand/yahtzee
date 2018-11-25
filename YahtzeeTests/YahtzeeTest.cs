51e608af447592b78b8e9cdd8ca4e79dfd8845b7
antonstrand <jag@antonstrand.se>
2018-11-25 20:21:26 +0100

    Red

--- YahtzeeTests/YahtzeeTest.cs
+++ YahtzeeTests/YahtzeeTest.cs
@@ -9,15 +9,14 @@ namespace YahtzeeTests
     [Theory]
     [InlineData(1, 2, 3, 4, 6)]
     [InlineData(1, 1, 3, 5, 5)]
-    public void ShouldNotAcceptInvalidInput(int v1, int v2, int v3, int v4, int v5)
-    {
+    public void ShouldNotAcceptInvalidInput(int v1, int v2, int v3, int v4, int v5) =>
       Assert.Throws<ArgumentException>(() => new category.Yahtzee(v1, v2, v3, v4, v5));
-    }
 
-    [Fact]
-    public void ShouldSetValueIfInputIsCorrect()
+    [Theory]
+    [InlineData(1, 1, 1, 1, 1)]
+    public void ShouldSetValueIfInputIsCorrect(int v1, int v2, int v3, int v4, int v5)
     {
-      var sut = new category.Yahtzee(1, 1, 1, 1, 1);
+      var sut = new category.Yahtzee(v1, v2, v3, v4, v5);
       Assert.Equal(50, sut.GetValue());
     }
   }
