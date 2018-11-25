3bb500991e95994959b8782100bfc8511e7eef6a
antonstrand <jag@antonstrand.se>
2018-11-25 12:40:02 +0100

    Green

--- Yahtzee/model/category/SmallStraight.cs
+++ Yahtzee/model/category/SmallStraight.cs
@@ -8,9 +8,15 @@ namespace Yahtzee.model.category
   {
     public SmallStraight(int v1, int v2, int v3, int v4, int v5)
     {
-      if (v1 != 1 || v2 != 2 || v3 != 3 || v4 != 4 || v5 != 5) throw new ArgumentException();
+      List<int> values = new List<int> { v1, v2, v3, v4, v5 };
+      if (IsInvalidInput(values)) throw new ArgumentException();
     }
 
     public int GetValue() => 15;
+
+    private bool IsInvalidInput(List<int> list) =>
+      list.OrderBy(v => v).Where((value, i) => value != (i + 1)).ToList().Count != 0;
+
+
   }
 }
