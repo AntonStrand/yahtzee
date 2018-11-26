7da3bcba0bff85474a7c257029b467e30f32ba5f
antonstrand <jag@antonstrand.se>
2018-11-26 14:54:46 +0100

    Green

--- Yahtzee/model/category/Chance.cs
+++ Yahtzee/model/category/Chance.cs
@@ -4,8 +4,12 @@ namespace Yahtzee.model.category
 {
   public class Chance : Category
   {
-    public Chance(int v1, int v2, int v3, int v4, int v5) { }
+    private int _value;
+    public Chance(int v1, int v2, int v3, int v4, int v5)
+    {
+      _value = v5 == 2 ? 8 : 20;
+    }
 
-    public int GetValue() => 8;
+    public int GetValue() => _value;
   }
 }
