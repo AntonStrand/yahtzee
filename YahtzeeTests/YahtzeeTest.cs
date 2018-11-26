bd8ad1e7ce7f11757ab8da40b45fa0d8c7555cdf
antonstrand <jag@antonstrand.se>
2018-11-26 09:07:19 +0100

    Red

--- YahtzeeTests/YahtzeeTest.cs
+++ YahtzeeTests/YahtzeeTest.cs
@@ -11,6 +11,7 @@ namespace YahtzeeTests
     [InlineData(1, 1, 3, 5, 5)]
     [InlineData(1, 1, 1, 5, 5)]
     [InlineData(1, 1, 1, 1, 5)]
+    [InlineData(1, 1, 2, 2, 2)]
     public void ShouldNotAcceptInvalidInput(int v1, int v2, int v3, int v4, int v5) =>
       Assert.Throws<ArgumentException>(() => new category.Yahtzee(v1, v2, v3, v4, v5));
 
