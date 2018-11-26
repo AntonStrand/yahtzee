79405d1e751c09eac3621b0bcad65ae617723c9e
antonstrand <jag@antonstrand.se>
2018-11-26 14:59:32 +0100

    Refactor

--- Yahtzee/model/category/Chance.cs
+++ Yahtzee/model/category/Chance.cs
@@ -5,10 +5,8 @@ namespace Yahtzee.model.category
   public class Chance : Category
   {
     private int _value;
-    public Chance(int v1, int v2, int v3, int v4, int v5)
-    {
+    public Chance(int v1, int v2, int v3, int v4, int v5) =>
       _value = v1 + v2 + v3 + v4 + v5;
-    }
 
     public int GetValue() => _value;
   }
