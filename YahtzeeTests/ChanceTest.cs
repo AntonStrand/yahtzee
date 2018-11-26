9904d20c506e4de463d267eb2f813f9c40c299f6
antonstrand <jag@antonstrand.se>
2018-11-26 14:55:51 +0100

    Red

--- YahtzeeTests/ChanceTest.cs
+++ YahtzeeTests/ChanceTest.cs
@@ -9,6 +9,7 @@ namespace YahtzeeTests
     [Theory]
     [InlineData(1, 1, 2, 2, 2, 8)]
     [InlineData(1, 6, 6, 3, 4, 20)]
+    [InlineData(3, 6, 1, 3, 5, 18)]
     public void ShouldCalculateValueBasedOnInput(int v1, int v2, int v3, int v4, int v5, int expected)
     {
       var sut = new Chance(v1, v2, v3, v4, v5);
