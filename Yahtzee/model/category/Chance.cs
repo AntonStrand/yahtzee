703494b6da16d0d73e3fb3f8cb82a797adff1235
antonstrand <jag@antonstrand.se>
2018-11-26 14:51:08 +0100

    Red

---
+++ Yahtzee/model/category/Chance.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Yahtzee.model.category
+{
+  public class Chance : Category
+  {
+    public Chance(int v1, int v2, int v3, int v4, int v5) { }
+
+    public int GetValue() => 0;
+  }
+}
---
+++ YahtzeeTests/ChanceTest.cs
@@ -0,0 +1,18 @@
+using System;
+using Xunit;
+using Yahtzee.model.category;
+
+namespace YahtzeeTests
+{
+  public class ChanceTest
+  {
+    [Theory]
+    [InlineData(1, 1, 2, 2, 2, 8)]
+    public void ShouldCalculateValueBasedOnInput(int v1, int v2, int v3, int v4, int v5, int expected)
+    {
+      var sut = new Chance(v1, v2, v3, v4, v5);
+      var actual = sut.GetValue();
+      Assert.Equal(expected, actual);
+    }
+  }
+}
