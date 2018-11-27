c916209e28c7e56df00b7bebdf861e6ef708db4f
antonstrand <jag@antonstrand.se>
2018-11-27 20:42:45 +0100

    Red

---
+++ Yahtzee/model/category/FirstSection.cs
@@ -0,0 +1,11 @@
+using System.Collections.Generic;
+
+namespace YahtzeeApp.model.category
+{
+  public class FirstSection : Category
+  {
+    public FirstSection(int valueType, List<int> values) { }
+
+    public int GetValue() => 0;
+  }
+}
---
+++ YahtzeeTests/model/category/FirstSectionTest.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using Xunit;
+using YahtzeeApp.model.category;
+
+namespace YahtzeeTests
+{
+  public class FirstSectionTest
+  {
+    [Fact]
+    public void ShouldNotAcceptNullValues() => Assert.Throws<ArgumentNullException>(() => new FirstSection(1, null));
+  }
+}
