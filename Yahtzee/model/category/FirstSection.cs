1b8b78b3d4a14066e477b02f3d48da75fb6451ba
antonstrand <jag@antonstrand.se>
2018-11-27 21:06:15 +0100

    Green

--- Yahtzee/model/category/FirstSection.cs
+++ Yahtzee/model/category/FirstSection.cs
@@ -12,7 +12,7 @@ namespace YahtzeeApp.model.category
     {
       if (dice == null) throw new ArgumentNullException();
       if (valueType == 1) _value = dice.GetValues().Where(x => x == 1).Sum();
-      else _value = dice.GetValues().Where(x => x == 2).Sum();
+      if (valueType == 2) _value = dice.GetValues().Where(x => x == 2).Sum();
     }
 
     public int GetValue() => _value;
