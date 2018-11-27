10d7941b671052a2d82963d4f7455091b79a4ecb
antonstrand <jag@antonstrand.se>
2018-11-27 20:58:41 +0100

    Refactor

--- YahtzeeTests/model/category/FirstSectionTest.cs
+++ YahtzeeTests/model/category/FirstSectionTest.cs
@@ -9,15 +9,16 @@ namespace YahtzeeTests
 {
   public class FirstSectionTest
   {
-    [Fact]
-    public void ShouldSumAllValuesOfSpecifiedType()
+    [Theory]
+    [InlineData(1, 2, 1, 2, 1, 3)]
+    public void ShouldSumAllValuesOfSpecifiedType(int v1, int v2, int v3, int v4, int v5, int expected)
     {
       var fakeDice = new Mock<Dice>();
-      fakeDice.Setup(d => d.GetValues()).Returns(new List<int> { 1, 2, 1, 1, 2 });
+      fakeDice.Setup(d => d.GetValues()).Returns(new List<int> { v1, v2, v3, v4, v5 });
 
-      var expected = 3;
       var sut = new FirstSection(1, fakeDice.Object);
       var actual = sut.GetValue();
+
       Assert.Equal(expected, actual);
     }
 
