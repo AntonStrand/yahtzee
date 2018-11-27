7eadd54f3987e1e75498cf8076e9c251bdf5ba79
antonstrand <jag@antonstrand.se>
2018-11-27 20:43:39 +0100

    Green

--- Yahtzee/model/category/FirstSection.cs
+++ Yahtzee/model/category/FirstSection.cs
@@ -1,10 +1,14 @@
+using System;
 using System.Collections.Generic;
 
 namespace YahtzeeApp.model.category
 {
   public class FirstSection : Category
   {
-    public FirstSection(int valueType, List<int> values) { }
+    public FirstSection(int valueType, List<int> values)
+    {
+      throw new ArgumentNullException();
+    }
 
     public int GetValue() => 0;
   }
