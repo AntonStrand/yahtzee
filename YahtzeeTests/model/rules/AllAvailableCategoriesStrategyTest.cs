3a30ef9582bf5f71058a9e2f7cafee832538a9d7
antonstrand <jag@antonstrand.se>
2018-11-28 19:31:52 +0100

    Refactor

--- YahtzeeTests/model/rules/AllAvailableCategoriesStrategyTest.cs
+++ YahtzeeTests/model/rules/AllAvailableCategoriesStrategyTest.cs
@@ -33,29 +33,31 @@ namespace YahtzeeTests
     [Fact]
     public void ShouldNotReturnPairIfAlreadyTaken()
     {
-      var diceValues = new List<int>() { 5, 5, 1, 1, 4 };
       var fakePlayer = new Mock<ScoreBoard>();
       fakePlayer.Setup(p => p.GetOccupiedCategories()).Returns(new List<Category> { new Pair(1, 1) });
+
+      var diceValues = new List<int>() { 5, 5, 1, 3, 4 };
       var fakeDice = new Mock<Dice>();
       fakeDice.Setup(d => d.GetValues()).Returns(diceValues);
 
       var sut = new AllAvailableCategoriesStrategy();
       var categories = sut.GetCategories(fakeDice.Object, fakePlayer.Object);
-      Assert.Equal(null, categories.Find(IsOfType<Pair>));
+      Assert.IsNotType<Pair>(categories.Find(IsOfType<Pair>));
     }
 
     [Fact]
     public void ShouldNotReturnPairIfAlreadyTakenButShouldReturnTwoPair()
     {
-      var diceValues = new List<int>() { 5, 1, 1, 2, 2 };
       var fakePlayer = new Mock<ScoreBoard>();
       fakePlayer.Setup(p => p.GetOccupiedCategories()).Returns(new List<Category> { new Pair(1, 1) });
+
+      var diceValues = new List<int>() { 5, 1, 1, 2, 2 };
       var fakeDice = new Mock<Dice>();
       fakeDice.Setup(d => d.GetValues()).Returns(diceValues);
 
       var sut = new AllAvailableCategoriesStrategy();
       var categories = sut.GetCategories(fakeDice.Object, fakePlayer.Object);
-      Assert.Equal(null, categories.Find(IsOfType<Pair>));
+      Assert.IsNotType<Pair>(categories.Find(IsOfType<Pair>));
       Assert.IsType<TwoPair>(categories.Find(IsOfType<TwoPair>));
     }
 
