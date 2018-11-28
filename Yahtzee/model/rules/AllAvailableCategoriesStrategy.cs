eb9e80c00cd86f24bc68a25e2c0adade030933f7
antonstrand <jag@antonstrand.se>
2018-11-28 11:01:50 +0100

    Refactory

--- Yahtzee/model/rules/AllAvailableCategoriesStrategy.cs
+++ Yahtzee/model/rules/AllAvailableCategoriesStrategy.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using YahtzeeApp.model.category;
 
-namespace YahtzeeApp.model
+namespace YahtzeeApp.model.rules
 {
   public class AllAvailableCategoriesStrategy
   {
