87273d01bb2a0a47e9859318bd7ca1b9ae6663f3
antonstrand <jag@antonstrand.se>
2018-11-28 11:04:49 +0100

    Red

---
+++ Yahtzee/model/Game.cs
@@ -0,0 +1,9 @@
+using YahtzeeApp.model.rules;
+
+namespace YahtzeeApp.model
+{
+  public class Game
+  {
+    public Game(AvailableCategoriesStrategy hjds) { }
+  }
+}
---
+++ YahtzeeTests/model/GameTest.cs
@@ -0,0 +1,15 @@
+using System;
+using Xunit;
+using YahtzeeApp.model;
+
+namespace YahtzeeTests
+{
+  public class GameTest
+  {
+    [Fact]
+    public void ShouldNotAcceptNullStrategy()
+    {
+      Assert.Throws<ArgumentNullException>(() => new Game(null));
+    }
+  }
+}
