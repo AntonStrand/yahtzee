968602a72613b95553539699a5859048db8a73be
antonstrand <jag@antonstrand.se>
2018-11-29 10:08:24 +0100

    Red

--- YahtzeeTests/model/category/AcesTest.cs
+++ YahtzeeTests/model/category/AcesTest.cs
@@ -11,6 +11,7 @@ namespace YahtzeeTests
   {
     [Theory]
     [InlineData(1, 1, 1, 1, 1, 5)]
+    [InlineData(1, 3, 3, 1, 3, 2)]
     public void ShouldSumAllAces(int v1, int v2, int v3, int v4, int v5, int expected)
     {
       var fakeDice = new Mock<Dice>();
