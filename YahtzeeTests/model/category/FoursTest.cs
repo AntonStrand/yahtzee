6f540294ac4685aeb3d0013bf9f9aad175c6ca25
antonstrand <jag@antonstrand.se>
2018-11-29 15:53:49 +0100

    Red

--- YahtzeeTests/model/category/FoursTest.cs
+++ YahtzeeTests/model/category/FoursTest.cs
@@ -10,6 +10,16 @@ namespace YahtzeeTests
   public class FoursTest
   {
     [Fact]
+    public void ShouldReturnSumOfAllFours()
+    {
+      var fakeDice = new Mock<Dice>();
+      fakeDice.Setup(d => d.GetValues()).Returns(new List<int> { 3, 4, 3, 2, 5 });
+
+      var sut = new Fours(fakeDice.Object);
+      Assert.Equal(4, sut.GetValue());
+    }
+
+    [Fact]
     public void ShouldImplementCategoryInterface()
     {
       var fakeDice = new Mock<Dice>();
