405697cde7355e5bbd84a2b24eab690ca83cb5de
antonstrand <jag@antonstrand.se>
2018-11-29 11:36:02 +0100

    Green

--- Yahtzee/model/category/Threes.cs
+++ Yahtzee/model/category/Threes.cs
@@ -1,11 +1,18 @@
 using System;
+using System.Linq;
 
 namespace YahtzeeApp.model.category
 {
   public class Threes : Category
   {
-    public Threes(Dice dice) { if (dice == null) throw new ArgumentNullException(); }
+    private int _value;
+    public Threes(Dice dice)
+    {
+      if (dice == null) throw new ArgumentNullException();
+      if (dice.GetValues() != null)
+        _value = dice.GetValues().Where(x => x == 3).ToList().Count == 2 ? 6 : 3;
+    }
 
-    public int GetValue() => 3;
+    public int GetValue() => _value;
   }
 }
