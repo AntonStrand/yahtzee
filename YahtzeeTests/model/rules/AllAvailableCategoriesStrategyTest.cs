8ff12a377d9c39cddbd07f8053398434235f209e
antonstrand <jag@antonstrand.se>
2018-11-29 17:17:34 +0100

    Red

--- YahtzeeTests/model/rules/AllAvailableCategoriesStrategyTest.cs
+++ YahtzeeTests/model/rules/AllAvailableCategoriesStrategyTest.cs
@@ -70,6 +70,11 @@ namespace YahtzeeTests
       AssertValueFromType<Aces>(new List<int>() { v1, v2, v3, v4, v5 }, expected);
 
     [Theory]
+    [InlineData(1, 2, 4, 2, 2, 6)]
+    public void ShouldReturnTwosWithCorrectValue(int v1, int v2, int v3, int v4, int v5, int expected) =>
+      AssertValueFromType<Twos>(new List<int>() { v1, v2, v3, v4, v5 }, expected);
+
+    [Theory]
     [InlineData(5, 6, 6, 2, 4, 12)]
     [InlineData(4, 1, 6, 2, 4, 8)]
     public void ShouldReturnPairWithCorrectValue(int v1, int v2, int v3, int v4, int v5, int expected) =>
