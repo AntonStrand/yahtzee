2184ccef22fa3fef07287f8125d1f92137e4b66d
antonstrand <jag@antonstrand.se>
2018-11-29 10:58:14 +0100

    Green

--- Yahtzee/model/category/Twos.cs
+++ Yahtzee/model/category/Twos.cs
@@ -1,11 +1,20 @@
 using System;
+using System.Linq;
 
 namespace YahtzeeApp.model.category
 {
   public class Twos : Category
   {
-    public Twos(Dice dice) { if (dice == null) throw new ArgumentNullException(); }
+    private int _value;
 
-    public int GetValue() => 10;
+    public Twos(Dice dice)
+    {
+      if (dice == null) throw new ArgumentNullException();
+      _value = dice.GetValues().Where(x => x == 2).ToList().Count == 5
+        ? 10
+        : 2;
+    }
+
+    public int GetValue() => _value;
   }
 }
