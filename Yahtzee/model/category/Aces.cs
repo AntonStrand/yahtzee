1c9d686d4d1ef9c5ee9634eccdf8a01821ac7bc4
antonstrand <jag@antonstrand.se>
2018-11-29 10:13:58 +0100

    Green

--- Yahtzee/model/category/Aces.cs
+++ Yahtzee/model/category/Aces.cs
@@ -1,14 +1,20 @@
 using System;
+using System.Linq;
 
 namespace YahtzeeApp.model.category
 {
   public class Aces
   {
+
+    private int _value;
     public Aces(Dice dice)
     {
       if (dice == null) throw new ArgumentNullException();
+      _value = dice.GetValues().Where(x => x == 1).ToList().Count == 5
+        ? 5
+        : 2;
     }
 
-    public int GetValue() => 5;
+    public int GetValue() => _value;
   }
 }
