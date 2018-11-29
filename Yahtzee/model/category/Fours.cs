c27ed1bf6039b511ccf46fa8ae68e0d6174d8d55
antonstrand <jag@antonstrand.se>
2018-11-29 15:52:16 +0100

    Green

--- Yahtzee/model/category/Fours.cs
+++ Yahtzee/model/category/Fours.cs
@@ -1,8 +1,10 @@
+using System;
+
 namespace YahtzeeApp.model.category
 {
   public class Fours : Category
   {
-    public Fours(Dice dice) { }
+    public Fours(Dice dice) { if (dice == null) throw new ArgumentNullException(); }
 
     public int GetValue() => 0;
   }
