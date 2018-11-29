52169ca96ea07d4227d2bfbe49d77cebe942e801
antonstrand <jag@antonstrand.se>
2018-11-29 16:57:15 +0100

    Refactor

--- Yahtzee/model/category/Sixes.cs
+++ Yahtzee/model/category/Sixes.cs
@@ -1,18 +1,7 @@
-using System;
-using System.Linq;
-
 namespace YahtzeeApp.model.category
 {
-  public class Sixes : Category
+  public class Sixes : FirstSection
   {
-    private int _value;
-
-    public Sixes(Dice dice)
-    {
-      if (dice == null) throw new ArgumentNullException();
-      _value = dice.GetValues().Where(x => x == 6).Sum();
-    }
-
-    public int GetValue() => _value;
+    public Sixes(Dice dice) : base(6, dice) { }
   }
 }
