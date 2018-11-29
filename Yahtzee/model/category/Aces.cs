fe6ee65ec193ecb9ca325949a31b3ff184da0a9b
antonstrand <jag@antonstrand.se>
2018-11-29 09:59:12 +0100

    Red

--- Yahtzee/model/category/Aces.cs
+++ Yahtzee/model/category/Aces.cs
@@ -5,5 +5,7 @@ namespace YahtzeeApp.model.category
   public class Aces
   {
     public Aces(Dice dice) { throw new ArgumentNullException(); }
+
+    public int GetValue() => 0;
   }
 }
--- YahtzeeTests/model/category/AcesTest.cs
+++ YahtzeeTests/model/category/AcesTest.cs
@@ -10,6 +10,18 @@ namespace YahtzeeTests
   public class AcesTest
   {
     [Fact]
+    public void ShouldSumAllAces()
+    {
+      var fakeDice = new Mock<Dice>();
+      fakeDice.Setup(d => d.GetValues()).Returns(new List<int> { 1, 1, 1, 1, 1 });
+
+      var sut = new Aces(fakeDice.Object);
+      var actual = sut.GetValue();
+
+      Assert.Equal(5, actual);
+    }
+
+    [Fact]
     public void ShouldNotAcceptNullValues() =>
       Assert.Throws<ArgumentNullException>(() => new Aces(null));
   }
