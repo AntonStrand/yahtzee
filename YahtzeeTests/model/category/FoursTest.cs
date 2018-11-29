0c771b7570e50755fc6c9dcd059bb32171dc5405
antonstrand <jag@antonstrand.se>
2018-11-29 16:05:07 +0100

    Refactor

--- YahtzeeTests/model/category/FoursTest.cs
+++ YahtzeeTests/model/category/FoursTest.cs
@@ -12,24 +12,21 @@ namespace YahtzeeTests
     [Fact]
     public void ShouldReturnSumOfAllFours()
     {
-      var sut = SetupSUT();
+      var sut = SetupSUT(3, 4, 3, 2, 5);
       Assert.Equal(4, sut.GetValue());
     }
 
     [Fact]
     public void ShouldReturnSumOfAllFours1()
     {
-      var fakeDice = new Mock<Dice>();
-      fakeDice.Setup(d => d.GetValues()).Returns(new List<int> { 4, 4, 4, 4, 4 });
-
-      var sut = new Fours(fakeDice.Object);
+      var sut = SetupSUT(4, 4, 4, 4, 4);
       Assert.Equal(20, sut.GetValue());
     }
 
     [Fact]
     public void ShouldImplementCategoryInterface()
     {
-      var sut = SetupSUT();
+      var sut = SetupSUT(3, 4, 3, 2, 5);
       Assert.True(sut is Category);
     }
 
@@ -37,10 +34,10 @@ namespace YahtzeeTests
     public void ShouldNotAcceptNullValues() =>
       Assert.Throws<ArgumentNullException>(() => new Fours(null));
 
-    private Fours SetupSUT()
+    private Fours SetupSUT(int v1, int v2, int v3, int v4, int v5)
     {
       var fakeDice = new Mock<Dice>();
-      fakeDice.Setup(d => d.GetValues()).Returns(new List<int> { 3, 4, 3, 2, 5 });
+      fakeDice.Setup(d => d.GetValues()).Returns(new List<int> { v1, v2, v3, v4, v5 });
 
       return new Fours(fakeDice.Object);
     }
