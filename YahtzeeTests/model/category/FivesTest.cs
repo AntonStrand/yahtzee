60f04255630f2df29c66b61da3f000003b9aaf3b
antonstrand <jag@antonstrand.se>
2018-11-29 16:26:43 +0100

    Refactor

--- YahtzeeTests/model/category/FivesTest.cs
+++ YahtzeeTests/model/category/FivesTest.cs
@@ -12,32 +12,33 @@ namespace YahtzeeTests
     [Fact]
     public void ShouldReturnSumOfAllFives()
     {
-      var fakeDice = new Mock<Dice>();
-      fakeDice.Setup(d => d.GetValues()).Returns(new List<int> { 5, 5, 5, 2, 3 });
-      var sut = new Fives(fakeDice.Object);
+      var sut = SetupSUT(5, 5, 5, 2, 3);
       Assert.Equal(expected: 15, actual: sut.GetValue());
     }
 
     [Fact]
     public void ShouldReturnSumOfAllFives1()
     {
-      var fakeDice = new Mock<Dice>();
-      fakeDice.Setup(d => d.GetValues()).Returns(new List<int> { 5, 5, 5, 5, 5 });
-      var sut = new Fives(fakeDice.Object);
+      var sut = SetupSUT(5, 5, 5, 5, 5);
       Assert.Equal(expected: 25, actual: sut.GetValue());
     }
 
     [Fact]
     public void ShouldImplementCategoryInterface()
     {
-      var fakeDice = new Mock<Dice>();
-      fakeDice.Setup(d => d.GetValues()).Returns(new List<int> { 5, 5, 5, 2, 3 });
-      var sut = new Fives(fakeDice.Object);
+      var sut = SetupSUT(5, 5, 5, 2, 3);
       Assert.True(sut is Category);
     }
 
     [Fact]
     public void ShouldNotAcceptNullValues() =>
       Assert.Throws<ArgumentNullException>(() => new Fives(null));
+
+    private Fives SetupSUT(int v1, int v2, int v3, int v4, int v5)
+    {
+      var fakeDice = new Mock<Dice>();
+      fakeDice.Setup(d => d.GetValues()).Returns(new List<int> { v1, v2, v3, v4, v5 });
+      return new Fives(fakeDice.Object);
+    }
   }
 }
