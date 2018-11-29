11dd331ee8e5cdd2eab394d9ab050073cf0fb320
antonstrand <jag@antonstrand.se>
2018-11-29 11:41:01 +0100

    Refactor

--- YahtzeeTests/model/category/ThreesTest.cs
+++ YahtzeeTests/model/category/ThreesTest.cs
@@ -9,35 +9,28 @@ namespace YahtzeeTests
 {
   public class ThreesTest
   {
-    [Fact]
-    public void ShouldReturnTheSumOfAllThrees()
-    {
-      var fakeDice = new Mock<Dice>();
-      fakeDice.Setup(d => d.GetValues()).Returns(new List<int> { 1, 2, 3, 4, 5 });
-      var sut = new Threes(fakeDice.Object);
-      Assert.Equal(3, sut.GetValue());
-    }
-
-    [Fact]
-    public void ShouldReturnTheSumOfAllThrees1()
-    {
-      var fakeDice = new Mock<Dice>();
-      fakeDice.Setup(d => d.GetValues()).Returns(new List<int> { 3, 2, 3, 4, 5 });
-      var sut = new Threes(fakeDice.Object);
-      Assert.Equal(6, sut.GetValue());
-    }
+    [Theory]
+    [InlineData(1, 2, 3, 4, 5, 3)]
+    [InlineData(3, 2, 3, 4, 5, 6)]
+    public void ShouldReturnTheSumOfAllThrees(int v1, int v2, int v3, int v4, int v5, int expected) =>
+      Assert.Equal(expected, actual: SetupSUT(v1, v2, v3, v4, v5).GetValue());
 
     [Fact]
     public void ShouldImplementCategoryInterface()
     {
-      var fakeDice = new Mock<Dice>();
-      fakeDice.Setup(d => d.GetValues()).Returns(new List<int> { 3, 2, 3, 4, 5 });
-      var sut = new Threes(fakeDice.Object);
+      var sut = SetupSUT(3, 2, 3, 4, 5);
       Assert.True(sut is Category);
     }
 
     [Fact]
     public void ShouldNotAcceptNullValues() =>
       Assert.Throws<ArgumentNullException>(() => new Threes(null));
+
+    private Threes SetupSUT(int v1, int v2, int v3, int v4, int v5)
+    {
+      var fakeDice = new Mock<Dice>();
+      fakeDice.Setup(d => d.GetValues()).Returns(new List<int> { v1, v2, v3, v4, v5 });
+      return new Threes(fakeDice.Object);
+    }
   }
 }
