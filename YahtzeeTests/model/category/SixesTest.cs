6e00cd5513950c8ff9ecdc6c37e540149605be35
antonstrand <jag@antonstrand.se>
2018-11-29 16:47:42 +0100

    Red

--- YahtzeeTests/model/category/SixesTest.cs
+++ YahtzeeTests/model/category/SixesTest.cs
@@ -10,6 +10,15 @@ namespace YahtzeeTests
   public class SixesTest
   {
     [Fact]
+    public void ShouldReturnSumOfAllSixes()
+    {
+      var fakeDice = new Mock<Dice>();
+      fakeDice.Setup(d => d.GetValues()).Returns(new List<int> { 6, 6, 6, 6, 1 });
+      var sut = new Sixes(fakeDice.Object);
+      Assert.Equal(24, sut.GetValue());
+    }
+
+    [Fact]
     public void ShouldImplementCategoryInterface()
     {
       var fakeDice = new Mock<Dice>();
