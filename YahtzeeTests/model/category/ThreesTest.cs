7bb84cb8d581563370743c5eb81a89941ff7cf99
antonstrand <jag@antonstrand.se>
2018-11-29 11:29:00 +0100

    Red

--- YahtzeeTests/model/category/ThreesTest.cs
+++ YahtzeeTests/model/category/ThreesTest.cs
@@ -10,6 +10,15 @@ namespace YahtzeeTests
   public class ThreesTest
   {
     [Fact]
+    public void ShouldReturnTheSumOfAllThrees()
+    {
+      var fakeDice = new Mock<Dice>();
+      fakeDice.Setup(d => d.GetValues()).Returns(new List<int> { 1, 2, 3, 4, 5 });
+      var sut = new Threes(fakeDice.Object);
+      Assert.Equal(3, sut.GetValue());
+    }
+
+    [Fact]
     public void ShouldImplementCategoryInterface()
     {
       var fakeDice = new Mock<Dice>();
