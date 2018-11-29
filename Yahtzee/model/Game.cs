d2e59e9139289ce92a2024980c66a957001d0426
antonstrand <jag@antonstrand.se>
2018-11-29 23:10:31 +0100

    Refactor

--- Yahtzee/model/Game.cs
+++ Yahtzee/model/Game.cs
@@ -7,22 +7,22 @@ namespace YahtzeeApp.model
   {
     private Dice _dice;
 
-    public Game(AvailableCategoriesStrategy categoryRule)
-    {
-      if (categoryRule == null) throw new ArgumentNullException();
-      _dice = initDice();
-    }
+    public Game(AvailableCategoriesStrategy categoryRule) => Init(categoryRule, null);
 
-    public Game(AvailableCategoriesStrategy categoryRule, Dice dice)
-    {
-      if (categoryRule == null) throw new ArgumentNullException();
-      _dice = dice != null ? dice : initDice();
-    }
+    public Game(AvailableCategoriesStrategy categoryRule, Dice dice) => Init(categoryRule, dice);
 
     public Dice GetDice() => _dice;
 
     public int GetNumberOfThrowsLeft() => 3;
 
-    private Dice initDice() => new DiceImplemented(new DieImplemented(), new DieImplemented(), new DieImplemented(), new DieImplemented(), new DieImplemented());
+    private void Init(AvailableCategoriesStrategy categoryRule, Dice dice)
+    {
+      if (IsNull(categoryRule)) throw new ArgumentNullException();
+      _dice = IsNull(dice) ? InitDice() : dice;
+    }
+
+    private Dice InitDice() => new DiceImplemented(new DieImplemented(), new DieImplemented(), new DieImplemented(), new DieImplemented(), new DieImplemented());
+
+    private bool IsNull(object o) => o == null;
   }
 }
