4daabe95122add22223b77e8b0eeb045f0bc24f4
antonstrand <jag@antonstrand.se>
2018-11-29 15:57:33 +0100

    Refactor

--- YahtzeeTests/model/category/FoursTest.cs
+++ YahtzeeTests/model/category/FoursTest.cs
@@ -12,25 +12,27 @@ namespace YahtzeeTests
     [Fact]
     public void ShouldReturnSumOfAllFours()
     {
-      var fakeDice = new Mock<Dice>();
-      fakeDice.Setup(d => d.GetValues()).Returns(new List<int> { 3, 4, 3, 2, 5 });
-
-      var sut = new Fours(fakeDice.Object);
+      var sut = SetupSUT();
       Assert.Equal(4, sut.GetValue());
     }
 
     [Fact]
     public void ShouldImplementCategoryInterface()
     {
-      var fakeDice = new Mock<Dice>();
-      fakeDice.Setup(d => d.GetValues()).Returns(new List<int> { 3, 4, 3, 2, 5 });
-
-      var sut = new Fours(fakeDice.Object);
+      var sut = SetupSUT();
       Assert.True(sut is Category);
     }
 
     [Fact]
     public void ShouldNotAcceptNullValues() =>
       Assert.Throws<ArgumentNullException>(() => new Fours(null));
+
+    private Fours SetupSUT()
+    {
+      var fakeDice = new Mock<Dice>();
+      fakeDice.Setup(d => d.GetValues()).Returns(new List<int> { 3, 4, 3, 2, 5 });
+
+      return new Fours(fakeDice.Object);
+    }
   }
 }
