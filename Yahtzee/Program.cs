2639e4fa30f610a7945f0dbc8827f4f5ce5b66aa
antonstrand <jag@antonstrand.se>
2018-11-30 12:55:37 +0100

    refactor

--- Yahtzee/Program.cs
+++ Yahtzee/Program.cs
@@ -13,17 +13,8 @@ namespace YahtzeeApp
     {
       var player = new Player();
 
-      var category = new AllAvailableCategoriesStrategy();
-
-      var die1 = new DieImplemented();
-      var die2 = new DieImplemented();
-      var die3 = new DieImplemented();
-      var die4 = new DieImplemented();
-      var die5 = new DieImplemented();
-
-      var dice = new DiceImplemented(die1, die2, die3, die4, die5);
-      var game = new Game(category, dice);
-      var diceView = new DiceView(dice);
+      var game = new Game(new AllAvailableCategoriesStrategy());
+      var diceView = new DiceView(game.GetDice());
       var view = new EnglishMainView(diceView);
       var controller = new MainController(view, player, game);
 
